69470e32f3063647ea03297a518c9684b8854ae5
MGYBY <53412286+MGYBY@users.noreply.github.com>
2021-01-26 21:55:55 -0500

    Add files via upload

---
+++ roll_wave_sims/normal_flow/b4step2.f90
@@ -0,0 +1,54 @@
+! ============================================
+subroutine b4step2(mbc,mx,my,meqn,q,xlower,ylower,dx,dy,t,dt,maux,aux,actualstep)
+! ============================================
+!
+! # called before each call to step
+! # use to set time-dependent aux arrays or perform other tasks.
+!
+! This particular routine sets negative values of q(1,i,j) to zero,
+! as well as the corresponding q(m,i,j) for m=1,meqn.
+! This is for problems where q(1,i,j) is a depth.
+! This should occur only because of rounding error.
+!
+! Also calls movetopo if topography might be moving.
+
+    use geoclaw_module, only: dry_tolerance
+    use geoclaw_module, only: g => grav
+    use topo_module, only: num_dtopo,topotime
+    use topo_module, only: aux_finalized
+    use topo_module, only: xlowdtopo,xhidtopo,ylowdtopo,yhidtopo
+
+    use amr_module, only: xlowdomain => xlower
+    use amr_module, only: ylowdomain => ylower
+    use amr_module, only: xhidomain => xupper
+    use amr_module, only: yhidomain => yupper
+    use amr_module, only: xperdom,yperdom,spheredom,NEEDS_TO_BE_SET
+
+    use storm_module, only: set_storm_fields
+
+    implicit none
+
+    ! Subroutine arguments
+    integer, intent(in) :: meqn
+    integer, intent(inout) :: mbc,mx,my,maux
+    real(kind=8), intent(inout) :: xlower, ylower, dx, dy, t, dt
+    real(kind=8), intent(inout) :: q(meqn,1-mbc:mx+mbc,1-mbc:my+mbc)
+    real(kind=8), intent(inout) :: aux(maux,1-mbc:mx+mbc,1-mbc:my+mbc)
+    logical, intent (in) :: actualstep
+
+    ! Local storage
+    integer :: index,i,j,k,dummy
+    real(kind=8) :: h,u,v,slope,hn,un
+
+    slope = 0.05011d0
+    hn = 0.00798d0
+    un = 1.03774d0
+
+    ! set inflow bcs
+    do j=1,my
+        q(1,1,j) = hn
+        q(2,1,j) = un*hn
+        q(3,1,j) = 0.0d0
+    enddo
+
+end subroutine b4step2
---
+++ roll_wave_sims/normal_flow/qinit.f90
@@ -0,0 +1,33 @@
+! qinit routine for parabolic bowl problem, only single layer
+subroutine qinit(meqn,mbc,mx,my,xlower,ylower,dx,dy,q,maux,aux)
+
+    use geoclaw_module, only: grav
+
+    implicit none
+
+    ! Subroutine arguments
+    integer, intent(in) :: meqn,mbc,mx,my,maux
+    real(kind=8), intent(in) :: xlower,ylower,dx,dy
+    real(kind=8), intent(inout) :: q(meqn,1-mbc:mx+mbc,1-mbc:my+mbc)
+    real(kind=8), intent(inout) :: aux(maux,1-mbc:mx+mbc,1-mbc:my+mbc)
+
+    ! ! Parameters for problem
+    integer :: i,j
+    real(kind=8) :: x,y,slope,hn,un,unlink
+
+    slope = 0.05011d0
+    hn = 0.00798d0
+    un = 1.03774d0
+    
+    do i=1-mbc,mx+mbc
+        x = xlower + (i - 0.5d0)*dx
+        do j=1-mbc,my+mbc
+            y = ylower + (j - 0.5d0) * dy
+            ! eta = sigma * h0 / a**2 * (2.d0 * x - sigma)
+            q(1,i,j) = hn
+            q(2,i,j) = un*hn
+            q(3,i,j) = 0.0d0
+        enddo
+    enddo
+    
+end subroutine qinit
