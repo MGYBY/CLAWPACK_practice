7340cba3188984ae92164236ccc67e06e1d6cc21
MGYBY <53412286+MGYBY@users.noreply.github.com>
2021-01-29 16:51:59 -0500

    Add files via upload
    Roll wave 2d simulation implemented by modifying the inflow b.c. (using Chezy firction law).

---
+++ roll_wave_sims/sinusoidal_wave/maketopo.py
@@ -0,0 +1,49 @@
+
+"""
+Module to create topo and qinit data files for this example.
+"""
+
+from __future__ import absolute_import
+from clawpack.geoclaw.topotools import Topography
+from numpy import *
+
+#from pyclaw.data import Data
+#probdata = Data('setprob.data')
+
+So = 0.05011
+hn = 0.00798
+un = 1.03774
+grav = 9.81
+cf = grav * So * 2 * hn / (un**2)
+
+domain_x = 40.0
+domain_y = 1.0
+
+def maketopo():
+    """
+    Output topography file for the entire domain
+    """
+    nxpoints=600
+    nypoints=15
+    xupper=domain_x
+    yupper=domain_y/2.0
+    xlower = 0.e0
+    ylower = -domain_y/2.0
+    outfile= "channel.topotype2"
+
+    topography = Topography(topo_func=topo)
+    topography.x = linspace(xlower,xupper,nxpoints)
+    topography.y = linspace(ylower,yupper,nypoints)
+    topography.write(outfile, topo_type=2, Z_format="%22.15e")
+
+
+def topo(x,y):
+    """
+    simple channel
+    """
+    z = -So*x
+    return z
+
+
+if __name__=='__main__':
+    maketopo()
